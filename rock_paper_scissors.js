8937179d6c1c431804abe8579d593ddd9f3207d4
Caroline Jaworsky <jaworskycaroline@gmail.com>
2021-08-22 23:09:52 +0100

    Started to create UI, made players and computers choice display, created buttons for the user to pick their choice and result display

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,74 +1,32 @@
+const game = document.querySelector('#game');
+const playerChoiceDisplay = document.createElement('h2');
+const comuterChoiceDisplay = document.createElement('h2');
+const resultDisplay = document.createElement('h2');
+
+playerChoiceDisplay.textContent = "Player Choice: ";
+comuterChoiceDisplay.textContent = "Computer Choice: ";
+resultDisplay.textContent = "Result: ";
+
+game.append(playerChoiceDisplay,comuterChoiceDisplay, resultDisplay);
+
+let choices = ["paper", "rock", "scissors"];
+let playerChoice;
+let computerChoice;
+
+choices.forEach(choice => {
+  const btn = document.createElement('button');
+  btn.textContent = choice;
+  btn.setAttribute('id', choice);
+  game.appendChild(btn);
+  btn.addEventListener('click', (e) => {
+    playerChoice = e.target.id
+    playerChoiceDisplay.textContent = "Player Choice: " + choice ;
+    computerPlay();
+  })
+});
+
 function computerPlay() {
-  let computerChoices = ["paper", "rock", "scissors"];
-  let computerChoice = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-  return computerChoice;
+  computerChoice = choices[Math.floor(Math.random()*choices.length)];
+  comuterChoiceDisplay.textContent = "Computer Choice: " + computerChoice;
 }
 
-let computerScore
-let playerScore
-let computerResult = 0
-let playerResult = 0
-
-function playRound(computerSelection, playerSelection) {
-  if (computerSelection === "rock" && playerSelection.toLowerCase() === "paper") {
-    alert("You win, paper beats rock!");
-    computerScore = 0;
-    playerScore = 1;
-  } 
-  else if (computerSelection === "paper" && playerSelection.toLowerCase() === "scissors") {
-    alert("You win, scissors beat paper!");
-    computerScore = 0;
-    playerScore = 1;
-  }
-  else if (computerSelection === "scissors" && playerSelection.toLowerCase() === "rock") {
-    alert("You win, rock beat scissors");
-    computerScore = 0; 
-    playerScore = 1;
-  }
-  else if (computerSelection === "rock" && playerSelection.toLowerCase() === "scissors") {
-    alert("You loose, rock beats scissors");
-    computerScore = 1;
-    playerScore = 0;
-  }
-  else if (computerSelection === "paper" && playerSelection.toLowerCase() === "rock") {
-    alert("You loose, paper beats rock");
-    computerScore = 1;
-    playerScore = 0;
-  }
-  else if (computerSelection === "scissors" && playerSelection.toLowerCase() === "paper") {
-    alert("You loose, scissors beats paper");
-    computerScore = 1;
-    playerScore = 0;
-  } else if (computerSelection === playerSelection.toLowerCase()) {
-    alert("even steven");
-    computerScore = 1;
-    playerScore = 1;
-  } else {
-    alert("Only paper, rock and scissors are valid options");
-  }
-}
-
-for (let i=1; i<=5; i++) {
-  computerPlay()
-  const playerSelection = prompt("Please enter your choice");
-  const computerSelection = computerPlay();
-  console.log(computerSelection);
-  console.log(playerSelection);
-  playRound(computerSelection, playerSelection);
-  computerResult += computerScore
-  playerResult += playerScore
-}
-
-function announceWinner () {
-  if (computerResult > playerResult){
-    alert("And the winner is COMPUTER!!!");
-  }
-  else if (computerResult < playerResult) {
-    alert("Congratulations YOU WON!!!");
-  }
-  else if(computerResult === playerResult) {
-    alert("WOW! It's a tie")
-  }
-}
-
-announceWinner();
